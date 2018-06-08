76c0dd41443985dfb0dec60007aa962cc60df2e4
Terrance Corley <terrancecorley@outlook.com>
2018-06-08 01:09:34 -0700

    added mobile styles for about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './About.css';
 
 export default function About() {
   return(
-    <section>
-      <div>
-        <p>Welcome! devSnips was designed for software developers by a software developer, allowing them to create notes that contain text, images and properly formatted code fast and on-the-fly. This is achieved by the user inputting <a href="https://guides.github.com/features/mastering-markdown" target="_blank" rel="noopener noreferrer">markdown</a> and having our snip (our word for note) minions convert that into plain HTML behind the scenes and then presenting that back to the user. Your snips are secure and inclusive as to completely protect your privacy and data. Happy coding!</p> 
+    <section className="about-section">
+      <div className="welcome-container">
+        <p className="welcome-text">Welcome! devSnips was designed for software developers by a software developer, allowing them to create notes fast and on-the-fly that contain text, images and properly formatted code.</p> 
+        
+        <p>This is achieved by the user inputting <a href="https://guides.github.com/features/mastering-markdown" target="_blank" rel="noopener noreferrer">markdown</a> and having our snip (our word for note) minions convert that into plain HTML behind the scenes and then presenting that back to the user. Your snips are secure and inclusive as to completely protect your privacy and data. Happy coding!</p> 
       </div>
     </section>
   );
--- src/components/Headers/AboutHeader/AboutHeader.js
+++ src/components/Headers/AboutHeader/AboutHeader.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './AboutHeader.css';
 
 export default function AboutHeader(props) {
   return (
-    <header role="banner">
+    <header role="banner" className="about-header">
       <Link to="/">Home</Link>
     </header>
   )
