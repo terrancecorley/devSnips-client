2dd49f4affa7022730b106070dd9dddcda588a77
Terrance Corley <terrancecorley@outlook.com>
2018-06-08 00:53:10 -0700

    added mobile styles for registration form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,9 +4,7 @@ import { Link, Redirect } from 'react-router-dom';
 import { required, checkEmpty, noSpaces, minLength, maxLength } from '../../validators';
 import { reduxForm, Field } from 'redux-form';
 import { registerUser } from '../../actions/user';
-
-//convert to redux form
-//redirect to homepage on successfull registration
+import './SignUp.css';
 
 export class SignUp extends Component {
   onSubmit(values){
@@ -19,33 +17,47 @@ export class SignUp extends Component {
     }
 
     return (
-      <form role="form"
+      <form role="form" className="registration-form"
         onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
-        <label htmlFor="username">Username</label>
+        <label 
+          htmlFor="username"
+          className="registration-label">
+          Username
+        </label>
         <Field
+          placeholder="Enter a username"
+          className="registration-input"
           component="input"
           type="text"
           id="username"
           name="username"
           validate={[required, checkEmpty, noSpaces, maxLength]}
           />
-          <label htmlFor="email">Email</label>
+        <label htmlFor="email" className="registration-label">Email</label>
         <Field
+          placeholder="Enter your email"
+          className="registration-input"
           component="input"
           type="email"
           id="email"
           name="email"
           validate={[required, checkEmpty, noSpaces, minLength, maxLength]}
           />
-        <label htmlFor="password">Password</label>
+        <label 
+          htmlFor="password"
+          className="registration-label">
+          Password
+        </label>
         <Field
+          placeholder="Enter a password"
+          className="registration-input"
           component="input"
           type="password"
           id="password"
           name="password"
           validate={[required, checkEmpty, noSpaces, minLength, maxLength]}
           />
-        <button type="submit">Register</button>
+        <button type="submit" className="registration-btn">Register</button>
         <p>Already have an account?<Link to="/"> Sign in</Link>.</p>
      </form>
     );
