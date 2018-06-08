416792097b03e227b0620ee7701ed3cda159a858
Terrance Corley <terrancecorley@outlook.com>
2018-06-07 22:02:50 -0700

    added cool 3-D effect to site logo

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,16 @@ import './Banner.css';
 export default function Banner(props) {
   return(
     <section className="banner-logo" role="region">
-      <h1 className="logo">devSnips</h1>
+      <h1 className="logo">
+        <span className="logo-d">d</span>
+        <span className="logo-e">e</span>
+        <span className="logo-v">v</span>
+        <span className="logo-S">S</span>
+        <span className="logo-n">n</span>
+        <span className="logo-i">i</span>
+        <span className="logo-p">p</span>
+        <span className="logo-s">s</span>
+      </h1>
     </section>
   );
 };
